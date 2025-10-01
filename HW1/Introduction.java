2a7c382f99453d5e3615a342b691ede66ac66a4c
24020304-NguyenVanTan <24020304@vnu.edu.vn>
2025-10-01 22:13:44 +0700

    Add files via upload

---
+++ HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Nguyễn Văn Tấn\t24020304\tINT 2204 6\t24020304-NguyenVanTan\t24020304@vnu.edu.vn");
+        for (int i = 9; i > 0; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around, ");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
